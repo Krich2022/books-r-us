c71c8cd4a4e11383dc6e58ed35ec65890f89952a
Krich2020 <keston@therichfamily.net>
2024-01-31 22:45:35 -0700

    updated app.jsx to work with apollo client

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,27 @@
-import './App.css';
-import { Outlet } from 'react-router-dom';
+import { ApolloProvider } from "@apollo/client";
+import { ApolloClient, InMemoryCache } from "@apollo/client";
+import "./App.css";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 
-import Navbar from './components/Navbar';
+import Navbar from "./components/Navbar";
+
+const client = new ApolloClient({
+  uri: "http://localhost:4000/graphql",
+  cache: new InMemoryCache(),
+});
 
 function App() {
   return (
-    <>
-      <Navbar />
-      <Outlet />
-    </>
+    <ApolloProvider client={client}>
+      <Router>
+        <div className="container">
+          <Navbar />
+          <Route exact path="/">
+            {/* Your other components go here */}
+          </Route>
+        </div>
+      </Router>
+    </ApolloProvider>
   );
 }
 
